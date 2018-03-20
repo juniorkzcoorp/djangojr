4a3aa9480bc642a4af70e5a663e474d7a8939945
Clodoaldo <jrkzuya@gmail.com>
2018-03-20 11:14:15 -0300

    ajustando README do projeto

--- djangoecommerce/settings.py
+++ djangoecommerce/settings.py
@@ -106,7 +106,7 @@ AUTH_PASSWORD_VALIDATORS = [
 # Internationalization
 # https://docs.djangoproject.com/en/1.9/topics/i18n/
 
-LANGUAGE_CODE = 'pt-br'
+LANGUAGE_CODE = 'en-US'
 
 TIME_ZONE = 'America/Recife'
 

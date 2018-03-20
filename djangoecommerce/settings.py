1beb4f52008a248337f3272f4c7820c5ce5d48fc
Clodoaldo <jrkzuya@gmail.com>
2018-03-20 11:21:55 -0300

    teste

--- djangoecommerce/settings.py
+++ djangoecommerce/settings.py
@@ -106,7 +106,7 @@ AUTH_PASSWORD_VALIDATORS = [
 # Internationalization
 # https://docs.djangoproject.com/en/1.9/topics/i18n/
 
-LANGUAGE_CODE = 'en-US'
+LANGUAGE_CODE = 'pt-br'
 
 TIME_ZONE = 'America/Recife'
 
